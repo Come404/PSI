a72df9e9e36e4b1a1d0dcb37416bc8abae20fe95
xuanqisun <sunxuanqi625@gmail.com>
2025-03-25 18:22:22 +0100

    modification de commentaires
    modification de commentaires dans le classe commentaires

--- Program.cs
+++ Program.cs
@@ -10,30 +10,30 @@ namespace PSI_Project_Perso
     {
         static void Main()
         {
-            // 读取图数据并创建图
+            // obtenir les donnees de graphe et creation deu graphe
             List<(int, int)> edges = ReadEdgesFromFile("soc-karate.mtx");
             Graphe graph = new Graphe(edges);
 
             Console.WriteLine("\nOrdre de graphe (le nombre de sommets qu'il contient) =  " + graph.OrdreDeGraphe());
             Console.WriteLine("\nTaille de graphe (le nombre d'arêtes du graphe) =  " + graph.TailleDeGraphe());
 
-            // 打印邻接矩阵
+            // imprimer la matrice d'adjacence
             graph.PrintAdjMatrix();
             Console.WriteLine("\nGraph est non-orienté ? " + (graph.EstNonOriente() ? "Oui" : "Non"));
 
-            // 执行 DFS 并输出结果
+            // executer le DFS et avoir le resultat
             Console.WriteLine("\nExécution de DFS:");
             graph.DFS_Main();
             Console.WriteLine(graph.ContientCycle ? "Le graphe contient un cycle." : "Le graphe ne contient pas de cycle.");
 
-            // 执行 BFS 并输出访问顺序
+            // executer le BFS, obtenir l'ordre de visite
             Console.WriteLine("\nExécution de BFS depuis le sommet 1:");
             List<int> bfsOrder = graph.BFS(1);
             graph.PrintBFSOrder(bfsOrder);
         }
 
         /// <summary>
-        /// 读取数据文件并转换为边的列表
+        ///  obtenir les donnees de graphe et obtenir le liste des arcs
         /// </summary>
         static List<(int, int)> ReadEdgesFromFile(string filePath)
         {
