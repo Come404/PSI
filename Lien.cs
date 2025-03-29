de0f92b2aab9e1d90d13b6d92764ce1f1269e149
Come404 <come.wozniak@edu.devinci.fr>
2025-03-29 12:36:42 +0100

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace PSI_Project_Perso
 {
-    internal class Lien
+    public class Lien
     {
+        public Noeud Noeud1 { get; }
+        public Noeud Noeud2 { get; }
+        /// <summary>
+        /// Créé un lien entre 2 noeuds noued1 et noeud2; constructeur naturel de la classe Lien
+        /// </summary>
+        public Lien(Noeud noeud1, Noeud noeud2)
+        {
+            Noeud1 = noeud1;
+            Noeud2 = noeud2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Noeud1.Id} - {Noeud2.Id}";
+        }
     }
 }
